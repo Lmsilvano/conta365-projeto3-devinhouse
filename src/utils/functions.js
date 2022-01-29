52b1e95fe89387600bdca14b95fd71c371d8dcfa
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2022-01-29 17:59:07 -0300

    .
    .

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,17 +1,21 @@
 const fileSystem = require('fs');
 
 function getData(fileName) {
-    const result = JSON.parse(fileSystem.readFileSync('src/database/' + fileName, 'utf8'));
-    return result
+    try {
+        const result = JSON.parse(fileSystem.readFileSync('src/database/' + fileName, 'utf8'));
+        return result
+    }
+    catch (err) {
+        return []
+    }
 }
 
 async function createOrUpdateData(fileName, data, updateDb = false) {
 
-
     if (!updateDb) {
         // Criação de novo dado
         const actualDb = getData(fileName)
-        const newDb = [...actualDb, { id: actualDb.length + 1, ...data }]
+        const newDb = !actualDb ? [{ id: 1, ...data }] : [...actualDb, { id: actualDb.length + 1, ...data }]
         return fileSystem.writeFileSync('src/database/' + fileName, JSON.stringify(newDb));
     }
     else {
