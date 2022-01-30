901cb5823adaeb7dafea13939772c7fd982e495f
Lmsilvano <75850731+Lmsilvano@users.noreply.github.com>
2022-01-30 14:42:09 -0300

    Update financial.routes.js

--- src/routes/v1/financial.routes.js
+++ src/routes/v1/financial.routes.js
@@ -3,7 +3,7 @@ const financialRoutes = express.Router()
 const financialController = require('../../controllers/financialController')
 const multer = require('multer')
 const upload = multer()
-financialRoutes.post('/v1/finance/:userId', upload.single('Financial Data'), financialController.insertFinancialData)
+financialRoutes.post('/v1/finance/:userId', upload.single('financialData'), financialController.insertFinancialData)
 financialRoutes.delete('/v1/finance/:userId/:financialId', financialController.deleteFinancialData)
 financialRoutes.get('/v1/finance/:userId', financialController.getFinancialDataByDate)
 
